7ab374115fda62bf8d7ccdf6fc4f2ea8fe2457a7
Ron Craig and Russell Hofvendahl <ron@>
2018-02-12 11:26:18 -0800

    pass first spec equilateral triangle

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,3 +2,16 @@
 require('pry')
 
 #shebang necessary?
+class Triangle
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def equilateral?
+    return (@side1 == @side2) && (@side1 == @side3)
+  end
+
+end
