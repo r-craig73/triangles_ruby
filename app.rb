af07101c59d5de1f06c68bfccda5de597e32577c
Ron Craig <ron.craig@comcast.net>
2018-11-17 21:35:58 -0800

    edit text and clean indentations

--- app.rb
+++ app.rb
@@ -1,22 +1,18 @@
-#!/home/linuxbrew/.linuxbrew/bin/env ruby
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/triangle')
 require('pry')
 
-
 get('/') do
-  @description = "Enter three sides"
+  @description = 'Enter three sides'
   erb(:input)
 end
 
-
-
 get('/output') do
-  @side_a = params.fetch("side-a").to_f
-  @side_b = params.fetch("side-b").to_f
-  @side_c = params.fetch("side-c").to_f
+  @side_a = params.fetch('side-a').to_f
+  @side_b = params.fetch('side-b').to_f
+  @side_c = params.fetch('side-c').to_f
   # binding.pry
   triangle = Triangle.new(@side_a, @side_b, @side_c)
   @type_response = triangle.type_response
