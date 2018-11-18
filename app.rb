f5ee5ad34dc6e36b8169804ed935a58cd22ddb0b
Ron Craig <ron.craig@comcast.net>
2018-11-17 21:41:14 -0800

    remove binding pry line

--- app.rb
+++ app.rb
@@ -13,7 +13,6 @@ get('/output') do
   @side_a = params.fetch('side-a').to_f
   @side_b = params.fetch('side-b').to_f
   @side_c = params.fetch('side-c').to_f
-  # binding.pry
   triangle = Triangle.new(@side_a, @side_b, @side_c)
   @type_response = triangle.type_response
   erb(:output)
